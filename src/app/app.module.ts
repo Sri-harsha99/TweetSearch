be02399f414fb6cc6d3b6fa6f1c716cadbe86f4d
harsha <venkatasriharsha.m17@iiits.in>
2022-09-22 20:13:10 -0500

    Test build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { A11yModule } from "@angular/cdk/a11y";
-import { ClipboardModule } from "@angular/cdk/clipboard";
-import { DragDropModule } from "@angular/cdk/drag-drop";
-import { PortalModule } from "@angular/cdk/portal";
-import { ScrollingModule } from "@angular/cdk/scrolling";
-import { CdkStepperModule } from "@angular/cdk/stepper";
-import { CdkTableModule } from "@angular/cdk/table";
-import { CdkTreeModule } from "@angular/cdk/tree";
 import { CommonModule } from "@angular/common";
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from "@angular/core";
 import { FlexLayoutModule } from "@angular/flex-layout";
