ff06448431998187a4eab853d9586f07a541215c
harsha <venkatasriharsha.m17@iiits.in>
2022-11-08 19:27:13 -0600

    Added github and linkedin links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'find-user';
   type = 'home';
+  
+  
+  link(type:any){
+    if(type == 'linkedin'){
+      window.open('https://www.linkedin.com/in/sriharshamaddirala/', "_blank");
+    }else{
+      window.open('https://github.com/Sri-harsha99', "_blank");
+    }
+  }
 }
